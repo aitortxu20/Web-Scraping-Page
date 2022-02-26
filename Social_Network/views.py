d3385d22015b86a54daf0d780d71977af35728b9
kali <aitortxuboy@gmail.com>
2022-02-26 15:22:04 -0500

    listos

--- Social_Network/views.py
+++ Social_Network/views.py
@@ -10,7 +10,7 @@ import os
 
 def home(request):
     BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-    return render(request, '/app/.heroku/Autentication/home.html')
+    return render(request, '/Social_Network/.heroku/Autentication/home.html')
 
 def get_element(request):
     BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
