835c6bab1fdde28c6b965f5866ccfa617935f8f1
kali <aitortxuboy@gmail.com>
2022-02-26 06:01:44 -0500

    listos6

--- Social_Network/settings.py
+++ Social_Network/settings.py
@@ -13,6 +13,10 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 import os
 import django_heroku
 django_heroku.settings(locals())
+MIDDLEWARE_CLASSES = (
+    # Simplified static file serving.
+    # https://warehouse.python.org/project/whitenoise/
+    'whitenoise.middleware.WhiteNoiseMiddleware',)
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
