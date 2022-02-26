dd460f03b65bd8c692120674cfc8fd212d2f0fc7
kali <aitortxuboy@gmail.com>
2022-02-26 06:59:36 -0500

    listos

--- Social_Network/settings.py
+++ Social_Network/settings.py
@@ -134,7 +134,7 @@ NULL = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+#STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
