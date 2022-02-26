2f4baa88fc8446a0c6cde56d71bbbadb2dfd1c68
kali <aitortxuboy@gmail.com>
2022-02-26 09:59:29 -0500

    listos

--- Social_Network/wsgi.py
+++ Social_Network/wsgi.py
@@ -11,6 +11,7 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 from whitenoise import WhiteNoise
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Social_Network.settings')
 
 application = get_wsgi_application()
