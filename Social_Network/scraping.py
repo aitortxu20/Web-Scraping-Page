b23549bb4942313104e7f552a21eb8dea495bec9
kali <aitortxuboy@gmail.com>
2022-02-28 08:51:57 -0500

    listos

--- Social_Network/scraping.py
+++ Social_Network/scraping.py
@@ -9,29 +9,31 @@ import os
 
 #HEADERS = {'User-Agent':'Mozilla/5.0 (X11; Linux x86_64; rv:78.0) Gecko/20100101 Firefox/78.0'}
 html_codes = ["""<head>
-                <meta charset="UTF-8">
-                <title>Compara Esta</title>
-                <style>
-                    body {
-                        background-image: url('https://visme.co/blog/wp-content/uploads/2017/07/50-Beautiful-and-Minimalist-Presentation-Backgrounds-036.jpg');
-                        background-repeat: no-repeat;
-                        background-attachment: fixed;
-                        background-size: cover;
-                    }
-                </style>
-            </head>""",]
+                    <meta charset="UTF-8">
+                    <title>Compara Esta</title>
+                    <style>
+                        body {
+                            background-image: url('https://visme.co/blog/wp-content/uploads/2017/07/50-Beautiful-and-Minimalist-Presentation-Backgrounds-036.jpg');
+                            background-repeat: no-repeat;
+                            background-attachment: fixed;
+                            background-size: cover;
+                        }
+                    </style>
+                </head>""", ]
+
 
 def amazon(element,html_codes):
 
-    HEADERS = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.102 Safari/537.36 Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.102 Safari/537.36 ' } #We use headers to see that the request for Amazon has been made by an human.
+    HEADERS = {
+        'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.102 Safari/537.36 Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.102 Safari/537.36 '}  # We use headers to see that the request for Amazon has been made by an human.
     amazon_content = {}
     ebay_content = {}
     ebay_content2 = {}
 
-    #First, we scrap in Amazon.
-    url = 'https://www.amazon.es/s?k='+element+'&__mk_es_ES=%C3%85M%C3%85%C5%BD%C3%95%C3%91&crid=2Z6LJDRVYPCRN&sprefix='+element+'%2Caps%2C116&ref=nb_sb_noss_1'
+    # First, we scrap in Amazon.
+    url = 'https://www.amazon.es/s?k=' + element + '&__mk_es_ES=%C3%85M%C3%85%C5%BD%C3%95%C3%91&crid=2Z6LJDRVYPCRN&sprefix=' + element + '%2Caps%2C116&ref=nb_sb_noss_1'
     webpage = requests.get(url, headers=HEADERS)
-    soup = BeautifulSoup(webpage.content,'lxml')
+    soup = BeautifulSoup(webpage.content, 'lxml')
     amazon_prices = []
     amazon_titles = []
     dict_amazon = []
@@ -41,19 +43,19 @@ def amazon(element,html_codes):
     buttons = []
     count_image = 0
     count_button = 0
-     #This is for the background image in /try/  path
+    # This is for the background image in /try/  path
 
-    #We scrap the title from Amazon.
+    # We scrap the title from Amazon.
     try:
-        #First we get the title of the element.
+        # First we get the title of the element.
         title = soup.find_all('span',
-                            attrs={'class': 'a-size-base-plus a-color-base a-text-normal'})
-        #Then we get the price.
+                              attrs={'class': 'a-size-base-plus a-color-base a-text-normal'})
+        # Then we get the price.
         price = soup.find_all('span',
-                          attrs={'class': 'a-price-whole'})
-        #Finally we get the image
+                              attrs={'class': 'a-price-whole'})
+        # Finally we get the image
         image = soup.find_all('img',
-                              attrs={'class':'s-image'})
+                              attrs={'class': 's-image'})
 
         for image_tag in image:
             count_image += 1
@@ -74,52 +76,40 @@ def amazon(element,html_codes):
             if contador_amazon < 4:
                 amazon_prices.append(prices.text + '$')
 
-
         for elemento in range(len(amazon_titles)):
             url = url + amazon_titles[elemento]
-            url = url.replace(' ','')
-            #We create an html code for each Amazon Search of the element.
+            url = url.replace(' ', '')
+            # We create an html code for each Amazon Search of the element.
             doc = """ <html>
-            <body>
-                <h2> AMAZON: </h2>
-                <div class="sg-col-4-of-12 s-result-item s-asin sg-col-4-of-16 AdHolder sg-col s-widget-spacing-small sg-col-4-of-20" data-asin="B07TTJR48G" data-index="1" data-uuid="ad272963-2afb-4a53-bdd9-d4ebb0b33d9e" data-height="100px">
-    
-                <span class="a-size-base-plus a-color-base a-text-normal">
-                    {}: <h2>Precio: {}</h2>
-                    <img src={} width="150px" >
-                  
-                </span>  
-                <form action={}>
-                    <button type="submit">🔗</button>
-                </form> 
-            </body>
-            </html>
-                                        
-            """.format(amazon_titles[elemento],amazon_prices[elemento],tags[elemento],url)
+                <body>
+                    <h2> AMAZON: </h2>
+                    <div class="sg-col-4-of-12 s-result-item s-asin sg-col-4-of-16 AdHolder sg-col s-widget-spacing-small sg-col-4-of-20" data-asin="B07TTJR48G" data-index="1" data-uuid="ad272963-2afb-4a53-bdd9-d4ebb0b33d9e" data-height="100px">
+
+                    <span class="a-size-base-plus a-color-base a-text-normal">
+                        {}: <h2>Precio: {}</h2>
+                        <img src={} width="150px" >
+
+                    </span>  
+                    <form action={}>
+                        <button type="submit">🔗</button>
+                    </form> 
+                </body>
+                </html>
+
+                """.format(amazon_titles[elemento], amazon_prices[elemento], tags[elemento], url)
 
             html_codes.append(doc)
             html_codes.append('\n')
-
-        #We make a dictionary where the key is the title and the value is the price.
-        dict_amazon = dict(zip(amazon_titles,amazon_prices))
-
-
-    except AttributeError:
-        title_string = 'NA'
-        print('product Title = ', title_string)
-        #return render(request,
-         #             '/app/Social_Network/Autentication/home.html')
-
-    #return html_codes,render(request, '/app/Social_Network/Autentication/home.html')
-    #return t.render(ctx)
-    return html_codes
+            return html_codes
+    except:
+        pass
 
 
-def ebay(element, html_codes):
+def ebay(element,html_codes):
 
-    #This function is the same we do for amazon but now for ebay.
-    HEADERS = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.102 Safari/537.36 Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.102 Safari/537.36 '}
-    url = 'https://www.ebay.es/sch/i.html?_from=R40&_trksid=p2380057.m570.l1313&_nkw='+element+'&_sacat=0'
+    HEADERS = {
+        'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.102 Safari/537.36 Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.102 Safari/537.36 '}
+    url = 'https://www.ebay.es/sch/i.html?_from=R40&_trksid=p2380057.m570.l1313&_nkw=' + element + '&_sacat=0'
     webpage2 = requests.get(url, headers=HEADERS)
     soup2 = BeautifulSoup(webpage2.content, 'lxml')
     ebay_prices = []
@@ -133,17 +123,16 @@ def ebay(element, html_codes):
 
     try:
         title_ebay = soup2.find_all('h3',
-                               attrs={'class': 's-item__title'})
-
+                                    attrs={'class': 's-item__title'})
 
         prices_ebay = soup2.find_all('span',
-                                attrs={'class': 's-item__price'})
+                                     attrs={'class': 's-item__price'})
 
         image = soup2.find_all('img',
-                              attrs={'class': 's-item__image-img'})
+                               attrs={'class': 's-item__image-img'})
 
         url_button = soup2.find_all('a',
-                              attrs={'class': 's-item__link'})
+                                    attrs={'class': 's-item__link'})
 
         for url_buttons in url_button:
             count_button += 1
@@ -166,8 +155,6 @@ def ebay(element, html_codes):
             contador_ebay += 1
             if contador_ebay < 6:
                 ebay_prices.append(price_in_ebay.text)
-
-
         for elemento in range(len(ebay_titles)):
             url = url + ebay_titles[elemento]
             url = url.replace(' ', '')
@@ -193,16 +180,9 @@ def ebay(element, html_codes):
             html_codes.append(doc)
             html_codes.append('\n')
 
-        dict_ebay = dict(zip(ebay_titles,ebay_prices))
+    except:
+        pass
 
-    except AttributeError:
-        title_string = 'NA'
-        print('product Title = ', title_string)
-        #return render(request,
-         #             '/app/Social_Network/Autentication/home.html')
-    #return html_codes,render(request,
-     #         '/app/Social_Network/Autentication/home.html')
-    
 
 
 
