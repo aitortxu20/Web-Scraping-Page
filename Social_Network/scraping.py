4b9d397839ca040c1286e2d3185f625de5edbecc
kali <aitortxuboy@gmail.com>
2022-03-06 08:45:16 -0500

    listos

--- Social_Network/scraping.py
+++ Social_Network/scraping.py
@@ -186,7 +186,25 @@ def ebay(element):
 
 
 def return_value(element):
-    amazon(element)
-    #ebay(element)
-    return html_codes
+    if len(html_codes) < 4:
+        amazon(element)
+        #ebay(element)
+        return html_codes
+    else:
+        html_codes.clear()
+        html_codes.append("""<head>
+                    <meta charset="UTF-8">
+                    <title>Compara Esta</title>
+                    <style>
+                        body {
+                            background-image: url('https://visme.co/blog/wp-content/uploads/2017/07/50-Beautiful-and-Minimalist-Presentation-Backgrounds-036.jpg');
+                            background-repeat: no-repeat;
+                            background-attachment: fixed;
+                            background-size: cover;
+                        }
+                    </style>
+                </head>""")
+        amazon(element)
+        return html_codes
+
 
